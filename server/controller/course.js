7aabae34b733a984531cc0ec0a847a8232856ef3
a1245582339 <1245582339@qq.com>
2018-04-17 22:55:42 +0800

    课程上传视频功能

--- server/controller/course.js
+++ server/controller/course.js
@@ -99,7 +99,7 @@ exports.UpdateCourse = function(req, res, next) {
         course_price: req.body.price,
         course_desp: req.body.desp,
         course_class_id: req.body.type,
-        video_url: url,
+        video_url: req.body.url,
         isBanner:0,
         cover: req.body.avatar,
         isDel: 0
